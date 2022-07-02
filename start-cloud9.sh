a5602773d3a3b1f4441c0bd119d8aa77926d8a02
Devin Dice <bpapilot@gmail.com>
2022-07-02 01:27:59 -0400

    Fixing ubuntu user

--- start-cloud9.sh
+++ start-cloud9.sh
@@ -1,6 +1,6 @@
 #if [ ! -z "$UPID" ]; then sudo usermod -u $UPID ubuntu; fi
 #if [ ! -z "$UGID" ]; then sudo usermod -g $UGID ubuntu; fi
-if [ ! -z "$C9_USER" ]; then sudo usermod -l $C9_USER ubuntu; fi
+if [ ! -z "$C9_USER" ]; then sudo usermod -l $C9_USER ubuntu; sudo useradd --non-unique -u 99 -g ubuntu -d /home/ubuntu -ms /bin/bash ubuntu; fi
 mkdir -p /workspace/.ubuntu/.standalone
 mkdir -p /workspace/.ubuntu/.c9
 ln -sf /workspace/.ubuntu/user.settings /home/ubuntu/.c9/user.settings
